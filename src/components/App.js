f4407ea7d09e84519d7f6bec4dc2ff70518b0e02
VipulTrimbake610 <vipultrimbake610@gmail.com>
2023-08-29 22:34:33 +0530

    submitting code-Tue Aug 29 2023 22:34:31 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
 const App = () => {
+  let myArr = [
+    {
+      id:"relativeListItem1",
+      name:"Hello"
+    },
+    {
+      id:"relativeListItem2",
+      name:"World"
+    },
+     {
+      id:"relativeListItem3",
+      name:"Rohan"
+    },
+    {
+      id:"relativeListItem4",
+      name:"Soham"
+    }
+  ]
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key="relativeList">
+               {
+                myArr.map((e)=>(
+                  <li key={e.id}>{e.name}</li>  
+                ))
+              }
+              </ol>
     </div>
   )
 }
